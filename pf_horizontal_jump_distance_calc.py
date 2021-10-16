fb036d0df8bd1b2bba3252786416872c5d5ed4eb
Zaela Rose <zaelarose02@gmail.com>
2021-10-16 13:01:16 -0500

    Finish long jump calc

--- pf_horizontal_jump_distance_calc.py
+++ pf_horizontal_jump_distance_calc.py
@@ -1,26 +1,53 @@
 # (c) Zaela Rose 2021
 
-# This calculator is to help calculate the minimum acrobatics check required to jump between two points in 1e Pathfinder
+# This calculator is to help calculate the minimum acrobatics check required to long jump between two points in
+# 1e Pathfinder
 
 # Pathfinder is a trademark of Pazio Inc., this calculator is unofficial and is in no way associated with Pazio Inc.
-
-# There is an assumption that the player has a falling speed of 500 ft per round without featherfall when over 10 feet
-# of vertical distance downwards is covered
-
-# The featherfall spell indicates that falling speeds are reduced to 60 ft per round, which will be assumed to be the
-# same speed when players are falling a vertical distance of less than 10 feet.
 
 # The apex of a long jump will be assumed to be a quarter of the horizontal distance from the player's starting point
 # to a point at the same height vertically in the direction the player is jumping
 
-MIN_FALLING_SPEED = 60
-MAX_FALLING_SPEED = 500
-
+# The height that Feather Fall mimics a fall from
 FEATHER_HEIGHT = 3
+
 
 def quadratic_upper(a, b, c):
     d = (b ** 2) - (4 * a * c)
     return (-b - d ** 0.5) / (2 * a)
+
+
+def featherfall_calc(height_difference, distance, max_height):
+    acrobatics = 5
+
+    distance_made = False
+
+    while not distance_made:
+        # horizontal distance (x-axis coord) where the player reaches max_height - FEATHER_HEIGHT
+        feather_point = quadratic_upper(-1, acrobatics, -acrobatics * (max_height - FEATHER_HEIGHT))
+
+        if isinstance(feather_point, complex):
+            acrobatics += 5
+            continue
+
+        # calculate slope at feather_point by using derivative of jump equation
+        slope = 1 - feather_point / (acrobatics / 2)
+
+        # use slope to find new linear equation that takes over the jump trajectory at the feather_point
+        y_intercept = (max_height - FEATHER_HEIGHT) - slope * feather_point
+
+        # use x = (y - b) / m = (height_difference - y_intercept) / slope to find new acrobatics check
+        effective_distance = int((height_difference - y_intercept) / slope)
+
+        if effective_distance >= distance:
+            distance_made = True
+        else:
+            acrobatics += 5
+
+    return acrobatics
+    # round up to nearest increment of 5 and return result
+    # return acrobatics if acrobatics % 5 == 0 else acrobatics + (5 - acrobatics % 5)
+
 
 def long_jump(height_difference, distance, featherfall):
     """
@@ -39,17 +66,15 @@ def long_jump(height_difference, distance, featherfall):
     :param featherfall: bool
     :return: int
     """
+    max_height = distance / 4
+
+    if distance < 5:
+        return -3  # Error, impossible distance
+
     if height_difference == 0:
 
-        max_height = distance / 4
-
         if featherfall and max_height > FEATHER_HEIGHT:
-            # horizontal distance (x-axis coord) where the player reaches max_height - FEATHER_HEIGHT
-            feather_point = quadratic_upper(-1, distance, -distance * (max_height - FEATHER_HEIGHT))
-
-            # calculate slope at feather_point by using derivative of jump equation
-            slope = 1 - feather_point / (distance / 2)
-
+            return featherfall_calc(height_difference, distance, max_height)
 
         # distances should always be in increments of 5 feet, though the ternary expression below was written on the
         # off chance a user enters a value not divisible by five.
@@ -58,10 +83,10 @@ def long_jump(height_difference, distance, featherfall):
     elif height_difference > 0:
 
         if height_difference >= distance:
-            return -1  # impossible jump
+            return -1  # Error, impossible jump
 
-        #if featherfall:
-            # calc
+        if featherfall and max_height - height_difference > FEATHER_HEIGHT:
+            return featherfall_calc(height_difference, distance, max_height)
 
         acrobatics = 5
         # x - x^2 / desired_distance where desired_distance is the distance in feet between the starting point and where
@@ -74,18 +99,38 @@ def long_jump(height_difference, distance, featherfall):
 
     elif height_difference < 0:
 
-        #if featherfall:
-            # calc
+        if featherfall and max_height - height_difference > FEATHER_HEIGHT:
+            return featherfall_calc(height_difference, distance, max_height)
 
         acrobatics = 5
         while distance - distance ** 2 / acrobatics < height_difference:
             acrobatics += 5
         return acrobatics
 
-
-
-
     else:
-        return -2
+        return -2  # Error, unknown error
 
-print(long_jump(0, 20, True))
+
+def main():
+    done = False
+    while not done:
+        h = int(input("Enter the height difference between the starting and ending points of the jump\n" +
+                      "(if jumping down enter a negative number): "))
+        d = int(input("Enter the horizontal distance between the starting and ending points of the jump: "))
+        feather_entered = False
+        while not feather_entered:
+            f = input("Is Feather Fall active? (y, n) : ")
+            if f in ["Y", "y", "Yes", "yes"]:
+                f = True
+                feather_entered = True
+            elif f in ["N", "n", "No", "no"]:
+                f = False
+                feather_entered = True
+        print("The minimum acrobatics check to make the jump is: %d" % (long_jump(h, d, f)))
+        cont = input("Continue? (y, n) : ")
+        if cont in ["N", "n"]:
+            done = True
+
+
+if __name__ == '__main__':
+    main()
